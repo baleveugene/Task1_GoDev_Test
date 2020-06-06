0beba72e2c471fad0345ae5900db300079087e27
balev.eugene <balev.eugene@gmail.com>
2020-06-06 22:20:35 +0300

    Automated scenario 2 (packageInfoUiChecks)

--- src/test/java/GoDevTestSuite.java
+++ src/test/java/GoDevTestSuite.java
@@ -1,4 +1,6 @@
 import godev.GoDevLandingPage;
+import godev.GoDevPackageInfoPage;
+import godev.GoDevSearchResultsPage;
 import godev.managers.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -32,6 +34,40 @@ public class GoDevTestSuite {
         );
 
         Assert.assertEquals(actualNavBarOptions, expectedNavBarOptions);
+    }
+
+    @Test
+    public void packageInfoUiChecks() {
+        String packageName = "github.com/mikekonan/protoc-gen-setter";
+        String searchCriteria = "setter";
+
+        GoDevLandingPage goDevPage = navigateToGoDevLandingPage();
+
+        GoDevSearchResultsPage searchResultsPage = goDevPage.getHeader().searchForAPackage(searchCriteria);
+        Assert.assertTrue(searchResultsPage.isOpened(), String.format("Page with search results for '%s' should be opened", searchCriteria));
+
+        GoDevPackageInfoPage packageInfoPage = searchResultsPage.navigateToPackageInfoPage(packageName);
+        Assert.assertTrue(packageInfoPage.isOpened(), String.format("Page for package '%s' should be opened", packageName));
+
+        List<String> actualNavTabNames = packageInfoPage.getDetailsNavTabNames();
+        List<String> expectedNavTabNames = Arrays.asList(
+                "Doc", "Overview", "Subdirectories", "Versions", "Imports", "Imported By", "Licenses"
+        );
+        Assert.assertEquals(
+                actualNavTabNames, expectedNavTabNames,
+                String.format("Page should contains following tabs: <%s>. But actual tabs are: <%s>", expectedNavTabNames, actualNavTabNames)
+        );
+
+        String actualPublishedDate = packageInfoPage.getDetailsHeaderPublishedDate();
+        String expectedPublishedDate = "Apr 13, 2020";
+        Assert.assertEquals(actualPublishedDate, expectedPublishedDate);
+
+        String actualVersion = packageInfoPage.getPackageVersion();
+        String expectedVersion = "v1.3.2";
+        Assert.assertEquals(actualVersion, expectedVersion);
+
+        String actualModule = packageInfoPage.getDetailsHeaderModuleName();
+        Assert.assertEquals(actualModule, packageName);
     }
 
     private GoDevLandingPage navigateToGoDevLandingPage() {
