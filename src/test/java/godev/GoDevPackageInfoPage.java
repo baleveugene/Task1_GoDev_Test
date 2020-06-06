c7c55f1891e6581e4ed0805911e34dc74e7547dc
balev.eugene <balev.eugene@gmail.com>
2020-06-06 22:20:01 +0300

    Added methods to check package info

--- src/test/java/godev/GoDevPackageInfoPage.java
+++ src/test/java/godev/GoDevPackageInfoPage.java
@@ -11,6 +11,8 @@ import java.util.stream.Collectors;
 
 public class GoDevPackageInfoPage extends GoDevBasePage {
     private static final Logger logger = LoggerFactory.getLogger(GoDevPackageInfoPage.class);
+    String XP_DETAILS_HEADER_LABEL = ".//div[@class='DetailsHeader-infoLabel']//span[contains(text(),'%s')]/following-sibling::*[1]";
+    private final By BY_DETAILS_NAV_TAB = By.className("DetailsNav-tab");
     private final By BY_PACKAGE_VERSION = By.className("DetailsHeader-version");
 
     public GoDevPackageInfoPage(WebDriver webDriver) {
@@ -28,7 +30,38 @@ public class GoDevPackageInfoPage extends GoDevBasePage {
         getPackageVersionElement().isDisplayed(5);
     }
 
+    public HtmlElement getDetailsHeaderPublishedDateElement() {
+        return new HtmlElement(webDriver, By.xpath(String.format(XP_DETAILS_HEADER_LABEL, "Published:")));
+    }
+
+    public HtmlElement getDetailsHeaderModuleNameElement() {
+        return new HtmlElement(webDriver, By.xpath(String.format(XP_DETAILS_HEADER_LABEL, "Module:")));
+    }
+
     public HtmlElement getPackageVersionElement() {
         return new HtmlElement(webDriver, BY_PACKAGE_VERSION);
+    }
+
+    public String getDetailsHeaderPublishedDate() {
+        logger.info("Getting package published date");
+        return getDetailsHeaderPublishedDateElement().getText();
+    }
+
+    public String getDetailsHeaderModuleName() {
+        logger.info("Getting package module name");
+        return getDetailsHeaderModuleNameElement().getText();
+    }
+
+    public String getPackageVersion() {
+        logger.info("Getting package version");
+        return getPackageVersionElement().getText();
+    }
+
+    public List<String> getDetailsNavTabNames() {
+        logger.info("Getting navigation tab names");
+        return webDriver.findElements(BY_DETAILS_NAV_TAB)
+                .stream()
+                .map(tab -> tab.getText().trim())
+                .collect(Collectors.toList());
     }
 }
